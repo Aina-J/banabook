e22643e076ee0cfb3fbef97a0ef855718c4def64
Aina-J <sujoo.aina@gmail.com>
2022-05-16 16:55:08 +0900

    스프링 프로젝트 (BANABOOK) 기본 세팅

--- src/test/java/com/banabook/persistence/DataSourceTest.java
+++ src/test/java/com/banabook/persistence/DataSourceTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/resources/spring/root-context.xml")
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 
 public class DataSourceTest {
 
