4eb277f97807142c9bfed9b2def4a059953d74e1
Aina-J <sujoo.aina@gmail.com>
2022-05-20 12:24:23 +0900

    selectAllInquiryPost 메서드 추가

--- src/main/java/com/banabook/web/domain/inquiry/mapper/InquiryDAO.java
+++ src/main/java/com/banabook/web/domain/inquiry/mapper/InquiryDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.dao.DataAccessException;
 
 import com.banabook.web.domain.inquiry.domain.InquiryDTO;
+import com.banabook.web.domain.review.domain.ReviewDTO;
 
 
 public interface InquiryDAO {
@@ -19,6 +20,15 @@ public interface InquiryDAO {
 	/* 문의 게시글 작성 */
 	public int insertMemberInquiry(InquiryDTO idto) throws DataAccessException;
 	
+	/* 로그인한 회원 본인이 작성한 문의글을 전부 불러오는 리스트 */
+	public List<InquiryDTO> selectAllInquiryPost(String id) throws DataAccessException;
+	
+	/* 구매자-판매자가 작성한 글과 답글 삭제 */
+	public int deleteInquiryToId(String id) throws DataAccessException;
+	
+	/* 상품에 해당하는 문의 게시판 전체 리스트 */
+	public List<InquiryDTO> fullListInquiryBoard(String code) throws DataAccessException;
+	
 	
 	/* 판매자 페이지용 - 판매자가 확인할 수 있는(본인이 판매하는) 상품 문의 게시판 */
 	public List<InquiryDTO> checkInquiryforSeller(String sellerId) throws DataAccessException;
@@ -26,10 +36,4 @@ public interface InquiryDAO {
 	/* 판매자 페이지용 - 상품 문의글에 답글 작성 */
 	public int insertSellerAnswerInquiry(InquiryDTO idto) throws DataAccessException;
 	
-
-	/* 구매자/판매자가 작성한 글과 답글 삭제 */
-	public int deleteInquiryToId(String id) throws DataAccessException;
-	
-	/* 상품에 해당하는 문의 게시판 전체 리스트 */
-	public List<InquiryDTO> fullListInquiryBoard(String code) throws DataAccessException;
 }
