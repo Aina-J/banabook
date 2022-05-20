04c8effdbddda0d89d1c73a06be1dcd5f12a17e2
hiimny <99874895+hiimny@users.noreply.github.com>
2022-05-20 13:46:53 +0900

    ProductController / 수정(대기) / productList 메서드 추가

--- src/main/java/com/banabook/web/domain/product/application/ProductController.java
+++ src/main/java/com/banabook/web/domain/product/application/ProductController.java
@@ -1,15 +1,33 @@
 package com.banabook.web.domain.product.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.banabook.web.domain.product.domain.ProductDTO;
+import com.banabook.web.domain.product.service.ProductService;
 
 @Controller
 public class ProductController {
 
+	@Autowired
+	ProductService productService;
 	
+	/* 해당 카테고리 상품목록에서 상품상세페이지로 이동 */
+	@RequestMapping(value="/product/list", method=RequestMethod.GET)
+	public String productList(Model model,
+			@RequestParam("code") String code
+			) {
+		
+		ProductDTO dto = productService.selectCodeProductOne(code);
+		
+		model.addAttribute("dto", dto);
+		
+		return "detail.view";
+	}
 
 }
