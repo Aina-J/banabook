780d7fe23930b3af6b6ee701beb3c45c382f32d5
Jang-YuJin <jangyj0912@naver.com>
2022-05-23 00:26:40 +0900

    inquiryControllerApi / 수정 (대기) / 상품상세페이지 review 아래에 문의 내역 보이게 하기위한 REST controller 수정 gnrlMember.xml / 수정 (완료) / selectMemberToID 쿼리문 수정 product.xml / 수정 (완료) / insertProduct 쿼리문 수정 detail.jsp / 수정 (대기) / 상품상세페이지 하단에 review 아래 문의 내역 보이도록 수정, 그런데 dom 위치가 이상해서 나중에 수정할 필요 있음 qna.css / 수정 (완료) / 상품상세페이지 하단에 있는 문의 내역 관련 css 수정

--- src/main/java/com/banabook/web/domain/inquiry/api/InquriyControllerApi.java
+++ src/main/java/com/banabook/web/domain/inquiry/api/InquriyControllerApi.java
@@ -12,55 +12,55 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banabook.web.domain.inquiry.domain.InquiryDTO;
 import com.banabook.web.domain.inquiry.service.InquiryService;
 import com.banabook.web.domain.product.domain.ProductDTO;
 import com.banabook.web.global.common.Paging;
-
 @RestController
 @RequestMapping("/inquiryApi")
 public class InquriyControllerApi {
 	@Autowired
 	InquiryService service;
-
-	@RequestMapping(value="/inquiry/{code}", method=RequestMethod.GET)
-	public Map goListPage(
-			HttpServletRequest req
-			,Model model
+	
+	@RequestMapping(value="/inquiryApi/{code}", method=RequestMethod.GET)
+	public @ResponseBody Map goInquiryListPage(
+			HttpServletRequest req,
+			Model model
 			,@PathVariable("code") String code
 			) {
-			
-		System.out.println("page : " + req.getParameter("page"));
-		List<InquiryDTO> InquiryList = service.selectProctDetailPage(code);
-		if(InquiryList.size() == 0) {
-			Map map = new HashMap();
-			map.put("data", "0");
-			return map;
-		} else {
-				Paging paging = new Paging(InquiryList.size());
-				paging.paging(Integer.parseInt(req.getParameter("page")));
-			
-				List<InquiryDTO> resultList = new ArrayList();
-				for(int i = paging.getBeginRow() - 1 ; i < paging.getEndRow() ; i++) {
-					resultList.add(InquiryList.get(i));
-				}
-				
-				Map map = new HashMap();
-				map.put("beginPage", paging.getBeginPage());
-				map.put("endPage", paging.getEndPage());
-				map.put("section", paging.getSection());
-				map.put("next", paging.isNext());
-				map.put("pre", paging.isPrev());
-				map.put("data", resultList);
-			
-
-			return map;
-		}
+		List<InquiryDTO> inquiryList = service.selectProctDetailPage(code);
+		System.out.println("--------여기 List사이즈------------" + inquiryList.size());
 		
+		
+		Map map = new HashMap();
+		
+		if(inquiryList.size() == 0) {
+	        map.put("data", "0");
+	        return map;
+	        
+		} else {
+			
+			Paging paging = new Paging(inquiryList.size());
+			System.out.println("paging은 : " + paging);
+			
+			paging.paging(Integer.parseInt(req.getParameter("page")));
+			
+			List<InquiryDTO> resultList = new ArrayList();
+			for(int i = paging.getBeginRow() - 1; i < paging.getEndRow(); i++) {
+				resultList.add(inquiryList.get(i));
+			}
+		
+	    	map.put("beginPage", paging.getBeginPage());
+	    	map.put("endPage", paging.getEndPage());
+	    	map.put("section", paging.getSection());
+	    	map.put("next", paging.isNext());
+	    	map.put("pre", paging.isPrev());
+	    	map.put("code", code);
+	    	map.put("data", resultList);
+	    	return map;
+	    }
 	}
-
-	
-
 }
