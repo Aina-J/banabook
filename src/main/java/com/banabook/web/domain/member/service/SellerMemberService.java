9cdedbedb99a6e5bcb2eeabe6dc0adf7954549af
최기범 <cgb1006@gmail.com>
2022-05-23 13:58:07 +0900

    SellerMemberService / 수정(완료) / 판매자 문의 답변 등록 메소드 추가 seller_qna_list.jsp / 수정(대기) / 판매자 문의리스트 진행중

--- src/main/java/com/banabook/web/domain/member/service/SellerMemberService.java
+++ src/main/java/com/banabook/web/domain/member/service/SellerMemberService.java
@@ -3,6 +3,8 @@ package com.banabook.web.domain.member.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.banabook.web.domain.inquiry.domain.InquiryDTO;
+import com.banabook.web.domain.inquiry.mapper.InquiryDAO;
 import com.banabook.web.domain.member.domain.MemberDTO;
 import com.banabook.web.domain.member.repository.SellerMemberDAO;
 
@@ -12,8 +14,15 @@ public class SellerMemberService {
 	@Autowired
 	SellerMemberDAO sellerDAO;
 	
+	@Autowired
+	InquiryDAO inquiryDAO;
+	
 	public int insertSellerMember(MemberDTO dto) {
 		return sellerDAO.insertSellerMember(dto);
+	}
+	
+	public int insertSellerAnswerInquiry(InquiryDTO dto) {
+		return inquiryDAO.insertSellerAnswerInquiry(dto);
 	}
 
 }
